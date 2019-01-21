844c71510962520705eb60467b0995cc3e4fe574
BLaZeKiLL <laldevashish@gmail.com>
2019-01-21 22:57:50 +0530

    Activation on startup View Time task added

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('TASK RUNNER STARTED');
+	console.log('TASK RUNNER STARTED');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.taskRunner', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	let mainCommand = vscode.commands.registerCommand('extension.taskRunner', () => {
 		vscode.window.showInformationMessage('Task Runner Activated');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(mainCommand);
+
+	let timeCommand = vscode.commands.registerCommand('extension.viewTime', () => {
+		vscode.window.showInformationMessage(`Current Time: ${new Date()}`);
+	});
+
+	context.subscriptions.push(timeCommand);
+
 }
 
 // this method is called when your extension is deactivated

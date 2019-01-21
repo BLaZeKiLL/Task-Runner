97b60f5b259f83968ba07f16c34eb1104b6047c8
BLaZeKiLL <laldevashish@gmail.com>
2019-01-22 01:11:04 +0530

    Script Loader done

--- src/main/ScriptLoader.ts
+++ src/main/ScriptLoader.ts
@@ -2,16 +2,16 @@ import { window, workspace } from "vscode";
 import * as fs from  'fs';
 
 export function loadScripts() {
-  console.log('Reading');
-  fs.readFile(`${workspace.workspaceFolders}/package.json`, (err, data) => {
-    if (err) {
-      console.log(err); 
-      throw err;
-    }
-    console.log('read');
-    const npm = JSON.parse(data.toString());
-    console.log(npm.scripts);
-    window.showInformationMessage('Scripts Loaded');
-  });
+  if (workspace.workspaceFolders !== undefined) {
+    fs.readFile(`${workspace.workspaceFolders[0].uri.fsPath}/package.json`, (err, data) => {
+      if (err) {
+        console.log(err); 
+        throw err;
+      }
+      const npm = JSON.parse(data.toString());
+      console.log(npm.scripts);
+      window.showInformationMessage('Scripts Loaded');
+    });
+  }
 }
 
